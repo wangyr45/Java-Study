e7e45a5470926acd30c90c43a369d5aa29d93fe7
wangyr45 <wangyuehanw1y2r3@163.com>
2018-11-25 10:41:43 +0800

    unit5-8-1

---
+++ unit_5/src/P131getInfo/ThisDemo02.java
@@ -0,0 +1,21 @@
+package P131getInfo;
+
+class Person{
+	private String name;
+	private int age;
+	public Person(String name ,int age) {
+		this.name = name;
+		this.age = age;
+	}
+	public String getInfo() {
+		return "姓名" + name + ",年龄" + age;
+	}
+}
+
+public class ThisDemo02{
+	public static void main(String[] args) {
+		Person per1 = new Person("张三",33);
+		System.out.println(per1.getInfo());
+	}
+}
+
